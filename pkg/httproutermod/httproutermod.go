cf66015c5581e5a5b1d1681dff9ce33f0e811335
munawwirul jamal <munawwirul.jamal@gmail.com>
2022-08-09 22:36:08 +0700

    feat (hrm): entend httprouter for midleware

---
+++ pkg/httproutermod/httproutermod.go
@@ -0,0 +1,45 @@
+// HttpRouterModified, embeded (baca: extend) httprouter
+// Tujuan utama mempersingkat handling http request dengan tetap memanfaatkan
+// handlerfunc untuk keperluan penggunaan middleware
+package httproutermod
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type RouterMod struct {
+	*httprouter.Router
+}
+
+func New() *RouterMod {
+	return &RouterMod{
+		Router: &httprouter.Router{
+			RedirectTrailingSlash:  true,
+			RedirectFixedPath:      true,
+			HandleMethodNotAllowed: true,
+			HandleOPTIONS:          true,
+		},
+	}
+}
+
+func (r *RouterMod) GETMOD(path string, handler http.HandlerFunc) {
+	r.Handler(http.MethodGet, path, handler)
+}
+
+func (r *RouterMod) POSTMOD(path string, handler http.HandlerFunc) {
+	r.Handler(http.MethodGet, path, handler)
+}
+
+func (r *RouterMod) PATCHMOD(path string, handler http.HandlerFunc) {
+	r.Handler(http.MethodPatch, path, handler)
+}
+
+func (r *RouterMod) PUTMOD(path string, handler http.HandlerFunc) {
+	r.Handler(http.MethodPut, path, handler)
+}
+
+func (r *RouterMod) DELMOD(path string, handler http.HandlerFunc) {
+	r.Handler(http.MethodDelete, path, handler)
+}
