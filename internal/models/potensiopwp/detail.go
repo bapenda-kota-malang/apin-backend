ce493d79abf65be88952c74cf2ed6db3d74ae758
Sultan Achmad <sultangendut@gmail.com>
2022-09-14 23:19:14 +0700

    fix (db); change model naming

--- internal/models/potensiopwp/detail.go
+++ internal/models/potensiopwp/detail.go
@@ -12,17 +12,17 @@ type DetailPotensiOp struct {
 }
 
 type Detail struct {
-	ID          uint           `json:"id" gorm:"primaryKey"`
-	PotensiopID uint           `json:"potensiop_id"`
-	JenisOp     string         `json:"jenisOp"`
-	JumlahOp    string         `json:"jumlahOp"`
-	TarifOp     string         `json:"tarifOp"`
-	UnitOp      string         `json:"unitOp"`
-	Notes       string         `json:"notes"`
-	CreatedAt   time.Time      `json:"createdAt"`
-	UpdatedAt   time.Time      `json:"updatedAt"`
-	DeletedAt   gorm.DeletedAt `json:"deletedAt" gorm:"index"`
-	Potensiop   PotensiOp
+	Id           uint           `json:"id" gorm:"primaryKey"`
+	Potensiop_Id uint           `json:"potensiop_id"`
+	JenisOp      string         `json:"jenisOp"`
+	JumlahOp     string         `json:"jumlahOp"`
+	TarifOp      string         `json:"tarifOp"`
+	UnitOp       string         `json:"unitOp"`
+	Notes        string         `json:"notes"`
+	CreatedAt    time.Time      `json:"createdAt"`
+	UpdatedAt    time.Time      `json:"updatedAt"`
+	DeletedAt    gorm.DeletedAt `json:"deletedAt" gorm:"index"`
+	Potensiop    PotensiOp      `gorm:"foreignKey:Potensi_Id"`
 }
 
 type DetailPotensiAirTanah struct {
--- internal/models/potensiopwp/potensi.go
+++ internal/models/potensiopwp/potensi.go
@@ -10,45 +10,45 @@ import (
 )
 
 type PotensiOp struct {
-	ID           uint                 `json:"id" gorm:"primaryKey"`
-	Golongan     rm.Golongan          `json:"golongan"`
-	RekeningID   uint                 `json:"rekening_id"`
-	Status       rm.StatusPendaftaran `json:"status"`
-	ClosingDate  time.Time            `json:"closingDate"`
-	OpeningDate  time.Time            `json:"openingDate"`
-	UserId       uint                 `json:"user_id"`
-	LuasBangunan string               `json:"luasBangunan" gorm:"size:50"`
-	JamBuka      string               `json:"jamBuka" gorm:"size:50"`
-	JamTutup     string               `json:"jamTutup" gorm:"size:50"`
-	Visitors     string               `json:"visitors" gorm:"size:50"`
-	OmsetOp      string               `json:"omsetOp" gorm:"size:50"`
-	Genset       string               `json:"genset" gorm:"size:10"`
-	AirTanah     string               `json:"airTanah" gorm:"size:10"`
-	VendorEtaxID uint                 `json:"vendorEtax_id"`
-	CreatedAt    time.Time            `json:"createdAt"`
-	UpdatedAt    time.Time            `json:"updatedAt"`
-	DeletedAt    gorm.DeletedAt       `json:"deletedAt" gorm:"index"`
-	Rekening     cm.Rekening
-	User         um.User
-	// VendorEtax   cm.VendorEtax
+	Id            uint                 `json:"id" gorm:"primaryKey"`
+	Golongan      rm.Golongan          `json:"golongan" validate:"required"`
+	Rekening_Id   uint                 `json:"rekening_id" validate:"required"`
+	Status        rm.StatusPendaftaran `json:"status" validate:"required"`
+	ClosingDate   time.Time            `json:"closingDate"`
+	OpeningDate   time.Time            `json:"openingDate"`
+	User_Id       uint                 `json:"user_id" validate:"required"`
+	LuasBangunan  string               `json:"luasBangunan" gorm:"size:50"`
+	JamBuka       string               `json:"jamBuka" gorm:"size:50"`
+	JamTutup      string               `json:"jamTutup" gorm:"size:50"`
+	Visitors      string               `json:"visitors" gorm:"size:50" validate:"required"`
+	OmsetOp       string               `json:"omsetOp" gorm:"size:50" validate:"required"`
+	Genset        string               `json:"genset" gorm:"size:10" validate:"required"`
+	AirTanah      string               `json:"airTanah" gorm:"size:10" validate:"required"`
+	VendorEtax_Id uint                 `json:"vendorEtax_id"`
+	CreatedAt     time.Time            `json:"createdAt"`
+	UpdatedAt     time.Time            `json:"updatedAt"`
+	DeletedAt     gorm.DeletedAt       `json:"deletedAt" gorm:"index"`
+	Rekening      cm.Rekening          `gorm:"foreignKey:Rekening_Id"`
+	User          um.User              `gorm:"foreignKey:User_Id"`
+	// VendorEtax    cm.VendorEtax        `gorm:"foreignKey:VendorEtax_Id"`
 }
 
 type Potensi struct {
-	ID          uint                `json:"id" gorm:"primaryKey"`
-	PotensiopID uint                `json:"Potensiop_id"`
-	Nama        string              `json:"nama" gorm:"size:50"`
-	Alamat      string              `json:"alamat" gorm:"size:50"`
-	RtRw        string              `json:"rt_rw" gorm:"size:10"`
-	KecamatanID uint                `json:"kecamatan_id"`
-	KelurahanID uint                `json:"kelurahan_id"`
-	Telp        string              `json:"telp" gorm:"size:20"`
-	Status      rm.StatusObjekPajak `json:"status"`
-	CreatedAt   time.Time           `json:"createdAt"`
-	UpdatedAt   time.Time           `json:"updatedAt"`
-	DeletedAt   gorm.DeletedAt      `json:"deletedAt" gorm:"index"`
-	Potensiop   PotensiOp
-	Kecamatan   cm.Kecamatan
-	Kelurahan   cm.Kelurahan
+	Id           uint                `json:"id" gorm:"primaryKey"`
+	Potensiop_Id uint                `json:"Potensiop_id"`
+	Nama         string              `json:"nama" gorm:"size:50"`
+	Alamat       string              `json:"alamat" gorm:"size:50"`
+	RtRw         string              `json:"rt_rw" gorm:"size:10"`
+	Kecamatan_Id uint                `json:"kecamatan_id"`
+	Kelurahan_Id uint                `json:"kelurahan_id"`
+	Telp         string              `json:"telp" gorm:"size:20"`
+	Status       rm.StatusObjekPajak `json:"status"`
+	CreatedAt    time.Time           `json:"createdAt"`
+	UpdatedAt    time.Time           `json:"updatedAt"`
+	DeletedAt    gorm.DeletedAt      `json:"deletedAt" gorm:"index"`
+	Potensiop    PotensiOp           `gorm:"foreignKey:Potensiop_Id"`
+	Kecamatan    cm.Kecamatan        `gorm:"foreignKey:Kecamatan_Id"`
+	Kelurahan    cm.Kelurahan        `gorm:"foreignKey:Kelurahan_Id"`
 }
 
 type PotensiPemilikWp struct {
