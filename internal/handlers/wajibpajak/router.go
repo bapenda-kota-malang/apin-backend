529c76ef2f548271795955842797229d7a299cf2
Sultan Achmad <sultangendut@gmail.com>
2022-09-28 09:21:47 +0700

    feat (wajibpajak): add endpoint area division

--- internal/handlers/wajibpajak/router.go
+++ internal/handlers/wajibpajak/router.go
@@ -6,6 +6,10 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
+	"github.com/bapenda-kota-malang/apin-backend/internal/handlers/bapenda/daerah"
+	"github.com/bapenda-kota-malang/apin-backend/internal/handlers/bapenda/kecamatan"
+	"github.com/bapenda-kota-malang/apin-backend/internal/handlers/bapenda/kelurahan"
+	"github.com/bapenda-kota-malang/apin-backend/internal/handlers/bapenda/provinsi"
 	"github.com/bapenda-kota-malang/apin-backend/internal/handlers/main/account"
 	er "github.com/bapenda-kota-malang/apin-backend/internal/handlers/main/errors"
 	"github.com/bapenda-kota-malang/apin-backend/internal/handlers/wajibpajak/auth"
@@ -40,6 +44,38 @@ func SetRoutes() http.Handler {
 	r.Route("/profile", func(r chi.Router) {
 		// 	r.Get("/", p.Login)   // EXECUTE
 		r.Patch("/{id}", profile.Update)
+	})
+
+	r.Route("/provinsi", func(r chi.Router) {
+		r.Post("/", provinsi.Create)
+		r.Get("/", provinsi.GetList)
+		r.Get("/{id}", provinsi.GetDetail)
+		r.Patch("/{id}", provinsi.Update)
+		r.Delete("/{id}", provinsi.Delete)
+	})
+
+	r.Route("/daerah", func(r chi.Router) {
+		r.Post("/", daerah.Create)
+		r.Get("/", daerah.GetList)
+		r.Get("/{id}", daerah.GetDetail)
+		r.Patch("/{id}", daerah.Update)
+		r.Delete("/{id}", daerah.Delete)
+	})
+
+	r.Route("/kecamatan", func(r chi.Router) {
+		r.Post("/", kecamatan.Create)
+		r.Get("/", kecamatan.GetList)
+		r.Get("/{id}", kecamatan.GetDetail)
+		r.Patch("/{id}", kecamatan.Update)
+		r.Delete("/{id}", kecamatan.Delete)
+	})
+
+	r.Route("/kelurahan", func(r chi.Router) {
+		r.Post("/", kelurahan.Create)
+		r.Get("/", kelurahan.GetList)
+		r.Get("/{id}", kelurahan.GetDetail)
+		r.Patch("/{id}", kelurahan.Update)
+		r.Delete("/{id}", kelurahan.Delete)
 	})
 
 	return r
