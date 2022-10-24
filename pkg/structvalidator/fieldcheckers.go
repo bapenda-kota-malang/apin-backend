eb8e755414daddae48bb5dbab8145bf534d731c3
Sultan Achmad <sultangendut@gmail.com>
2022-10-24 23:21:13 +0700

    feat (validator): add validation for nik, nohp

--- pkg/structvalidator/fieldcheckers.go
+++ pkg/structvalidator/fieldcheckers.go
@@ -23,6 +23,8 @@ func init() {
 	RegisterFieldChecker("maxLength", maxLengthTagValidator)
 	RegisterFieldChecker("validemail", emailValidator)
 	RegisterFieldChecker("base64", base64Validator)
+	RegisterFieldChecker("nik", nikValidator)
+	RegisterFieldChecker("nohp", noHpValidator)
 }
 
 ///// Field checkers
@@ -108,6 +110,41 @@ func base64Validator(val reflect.Value, exptVal string) error {
 		return nil
 	}
 	re := regexp.MustCompile(`^(data:)([\w\/\+-]*)(;charset=[\w-]+|;base64){0,1},([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$`)
+
+	if !re.MatchString(h.ValStringer(val)) {
+		return errors.New("harus memiliki format base64")
+	}
+
+	return nil
+}
+
+/*
+Artinya
+validasi 6 karakter pertama, 2 angka pertama sesuai dengan format provinsi di Indonesia, 2 karakter berikutnya angka, dan 2 karakter selanjutnya juga angka.
+6 karakter berikutnya mengikuti format tanggal lahir 01..31 (untuk laki-laki) atau 41..71 (untuk perempuan), bulan lahir 01-12, dan tahun lahir 2 angka
+4 karakter terakhir adalah angka sequence.
+*/
+func nikValidator(val reflect.Value, exptVal string) error {
+	if val.Kind() == reflect.Pointer && val.IsNil() {
+		return nil
+	}
+	re := regexp.MustCompile(`^(1[1-9]|21|[37][1-6]|5[1-3]|6[1-5]|[89][12])\d{2}\d{2}([04][1-9]|[1256][0-9]|[37][01])(0[1-9]|1[0-2])\d{2}\d{4}$`)
+
+	if !re.MatchString(h.ValStringer(val)) {
+		return errors.New("harus memiliki format base64")
+	}
+
+	return nil
+}
+
+// validate no hp indonesia
+//
+// dianggap valid untuk: +62897123456, 0897123456.
+func noHpValidator(val reflect.Value, exptVal string) error {
+	if val.Kind() == reflect.Pointer && val.IsNil() {
+		return nil
+	}
+	re := regexp.MustCompile(`^(\+62|62|0)8[1-9][0-9]{6,9}$`)
 
 	if !re.MatchString(h.ValStringer(val)) {
 		return errors.New("harus memiliki format base64")
