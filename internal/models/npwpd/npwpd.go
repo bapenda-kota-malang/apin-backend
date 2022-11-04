d858e728cedfe1d1bd2420f14843d486c152b0ce
Dwi Atmoko <dwiatmokop@gmail.com>
2022-11-04 15:38:40 +0700

    fix (regnpwpd) : solving conflict

--- internal/models/npwpd/npwpd.go
+++ internal/models/npwpd/npwpd.go
@@ -20,7 +20,7 @@ type Npwpd struct {
 	Nomor             int                `json:"nomor"`
 	TanggalPengukuhan *time.Time         `json:"tanggalPengukuhan"`
 	TanggalNpwpd      *time.Time         `json:"tanggalNpwpd"`
-	Npwpd             *string            `json:"npwpd" gorm:"size:22"`
+	Npwpd             *string            `json:"npwpd" gorm:"unique;size:22"`
 	Status            t.Status           `json:"status"`
 	TanggalTutup      *time.Time         `json:"tanggalTutup"`
 	TanggalBuka       *time.Time         `json:"tanggalBuka"`
@@ -40,7 +40,7 @@ type Npwpd struct {
 	JamBukaUsaha      *string            `json:"jamBukaUsaha" gorm:"size:50"`
 	JamTutupUsaha     *string            `json:"jamTutupUsaha" gorm:"size:50"`
 	Pengunjung        *string            `json:"pengunjung" gorm:"size:50"`
-	FotoKtp           string             `json:"fotoKtp" gorm:"size:50"`
+	FotoKtp           string             `json:"fotoKtp" gorm:"size:2048"`
 	SuratIzinUsaha    string             `json:"suratIzinUsaha" gorm:"size:2048"`
 	LainLain          string             `json:"lainLain" gorm:"size:2048"`
 	FotoObjek         string             `json:"fotoObjek" gorm:"size:2048"`
