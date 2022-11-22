4ab2a603f75ad444ec77937ac6122c3e7d2f48a6
Sultan Achmad <sultangendut@gmail.com>
2022-11-22 17:52:23 +0700

    fix(pendataan): kecamatan -> kelurahan idk why

--- internal/models/potensiopwp/potensinarahubung/potensinarahubung.go
+++ internal/models/potensiopwp/potensinarahubung/potensinarahubung.go
@@ -13,13 +13,13 @@ type PotensiNarahubung struct {
 	Alamat       string                  `json:"alamat" gorm:"size:50"`
 	RtRw         string                  `json:"rtRw" gorm:"size:10"`
 	Daerah_Id    uint                    `json:"daerah_id"`
-	Kecamatan_Id *uint                   `json:"kecamatan_id"`
+	Kelurahan_Id *uint                   `json:"kelurahan_id"`
 	Telp         *string                 `json:"telp" gorm:"size:20"`
 	Status       t.StatusBL              `json:"status"`
 	Nik          string                  `json:"nik" gorm:"size:20"`
 	Email        *string                 `json:"email"`
 	Daerah       *areadivision.Daerah    `json:"daerah,omitempty" gorm:"foreignKey:Daerah_Id"`
-	Kecamatan    *areadivision.Kecamatan `json:"kecamatan,omitempty" gorm:"foreignKey:Kecamatan_Id"`
+	Kelurahan    *areadivision.Kelurahan `json:"kelurahan,omitempty" gorm:"foreignKey:Kelurahan_Id"`
 	gormhelper.DateModel
 }
 
@@ -29,7 +29,7 @@ type CreateDto struct {
 	Alamat       string     `json:"alamat" validate:"required"`
 	RtRw         string     `json:"rtRw"`
 	Daerah_Id    uint       `json:"daerah_id" validate:"required;min=1"`
-	Kecamatan_Id *uint      `json:"kecamatan_id" validate:"min=1"`
+	Kelurahan_Id *uint      `json:"kelurahan_id" validate:"min=1"`
 	Telp         string     `json:"telp" validate:"required"`
 	Status       t.StatusBL `json:"status" validate:"required"`
 	Nik          string     `json:"nik" validate:"nik"`
@@ -42,7 +42,7 @@ type UpdateDto struct {
 	Alamat       *string     `json:"alamat"`
 	RtRw         *string     `json:"rtRw"`
 	Daerah_Id    *uint       `json:"daerah_id" validate:"min=1"`
-	Kecamatan_Id *uint       `json:"kecamatan_id" validate:"min=1"`
+	Kelurahan_Id *uint       `json:"kelurahan_id" validate:"min=1"`
 	Telp         *string     `json:"telp"`
 	Status       *t.StatusBL `json:"status"`
 	Nik          *string     `json:"nik" validate:"nik"`
