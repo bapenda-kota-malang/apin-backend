00d2d615043d5daeaea7d0a90a2b94d6abb1ff31
Dwi Atmoko <dwiatmokop@gmail.com>
2022-12-01 12:27:36 +0700

    fix (undanganpemeriksaan): change model, add pukul and tempat field

--- internal/models/undanganpemeriksaan/undanganpemeriksaan.go
+++ internal/models/undanganpemeriksaan/undanganpemeriksaan.go
@@ -3,63 +3,68 @@ package undanganpemeriksaan
 import (
 	"time"
 
-	msp "github.com/bapenda-kota-malang/apin-backend/internal/models/suratpemberitahuan"
+	mn "github.com/bapenda-kota-malang/apin-backend/internal/models/npwpd"
 	mu "github.com/bapenda-kota-malang/apin-backend/internal/models/user"
 	gh "github.com/bapenda-kota-malang/apin-backend/pkg/gormhelper"
+	"gorm.io/datatypes"
 )
 
 type UndanganPemeriksaan struct {
-	Id                       uint64                  `json:"id" gorm:"primaryKey"`
-	JenisPajak               int                     `json:"jenisPajak"`
-	SuratPemberitahuan_Id    *uint64                 `json:"suratPemberitahuan_id"`
-	SuratPembertahuan        *msp.SuratPemberitahuan `json:"suratPemberitahuan,omitempty" gorm:"foreignKey:SuratPemberitahuan_Id"`
-	SuratPemberitahuanPbb_Id *uint64                 `json:"suratPemberitahuanPbb_id"`
-	Tanggal                  *time.Time              `json:"tanggal"`
-	Keperluan                *string                 `json:"keperluan"`
-	Petugas_User_Id          *uint64                 `json:"petugas_user_id"`
-	Petugas                  *mu.User                `json:"petugas,omitempty" gorm:"foreignKey:Petugas_User_Id"`
-	Status                   Status                  `json:"status"`
-	StatusKirim              StatusKirim             `json:"statusKirim"`
-	NoSuratUndangan          *string                 `json:"noSuratUndangan"`
+	Id              uint64          `json:"id" gorm:"primaryKey"`
+	JenisPajak      int             `json:"jenisPajak"`
+	Tanggal         *time.Time      `json:"tanggal"`
+	Pukul           *datatypes.Time `json:"pukul"`
+	Tempat          *string         `json:"tempat"`
+	Keperluan       *string         `json:"keperluan"`
+	Petugas_User_Id *uint64         `json:"petugas_user_id"`
+	Petugas         *mu.User        `json:"petugas,omitempty" gorm:"foreignKey:Petugas_User_Id"`
+	Status          Status          `json:"status"`
+	StatusKirim     StatusKirim     `json:"statusKirim"`
+	NoSuratUndangan *string         `json:"noSuratUndangan"`
+	Npwpd_Id        uint64          `json:"npwpd_id"`
+	Npwpd           *mn.Npwpd       `json:"npwpd,omitempty" gorm:"foreignKey:Npwpd_Id"`
 	gh.DateModel
 }
 
 type CreateDto struct {
-	JenisPajak               int         `json:"jenisPajak"`
-	SuratPemberitahuan_Id    *uint64     `json:"suratPemberitahuan_id"`
-	SuratPemberitahuanPbb_Id *uint64     `json:"suratPemberitahuanPbb_id"`
-	Tanggal                  *string     `json:"tanggal"`
-	Keperluan                *string     `json:"keperluan"`
-	Petugas_User_Id          *uint64     `json:"petugas_user_id"`
-	Status                   Status      `json:"status"`
-	StatusKirim              StatusKirim `json:"statusKirim"`
-	NoSuratUndangan          *string     `json:"noSuratUndangan"`
+	JenisPajak      int         `json:"jenisPajak" validate:"required"`
+	Tanggal         *string     `json:"tanggal" validate:"required"`
+	Pukul           string      `json:"pukul" validate:"required"`
+	Tempat          *string     `json:"tempat" validate:"required"`
+	Keperluan       *string     `json:"keperluan" validate:"required"`
+	Petugas_User_Id *uint64     `json:"petugas_user_id"`
+	Status          Status      `json:"status"`
+	StatusKirim     StatusKirim `json:"statusKirim"`
+	NoSuratUndangan *string     `json:"noSuratUndangan"`
+	Npwpd_Id        uint64      `json:"npwpd_id" validate:"required"`
 }
 
 type UpdateDto struct {
-	JenisPajak               int         `json:"jenisPajak"`
-	SuratPemberitahuan_Id    *uint64     `json:"suratPemberitahuan_id"`
-	SuratPemberitahuanPbb_Id *uint64     `json:"suratPemberitahuanPbb_id"`
-	Tanggal                  *string     `json:"tanggal"`
-	Keperluan                *string     `json:"keperluan"`
-	Petugas_User_Id          *uint64     `json:"petugas_user_id"`
-	Status                   Status      `json:"status"`
-	StatusKirim              StatusKirim `json:"statusKirim"`
-	NoSuratUndangan          *string     `json:"noSuratUndangan"`
+	JenisPajak      int         `json:"jenisPajak"`
+	Tanggal         *string     `json:"tanggal"`
+	Pukul           string      `json:"pukul"`
+	Tempat          *string     `json:"tempat"`
+	Keperluan       *string     `json:"keperluan"`
+	Petugas_User_Id *uint64     `json:"petugas_user_id"`
+	Status          Status      `json:"status"`
+	StatusKirim     StatusKirim `json:"statusKirim"`
+	NoSuratUndangan *string     `json:"noSuratUndangan"`
+	Npwpd_Id        uint64      `json:"npwpd_id"`
 }
 
 type FilterDto struct {
-	JenisPajak               int         `json:"jenisPajak"`
-	SuratPemberitahuan_Id    *uint64     `json:"suratPemberitahuan_id"`
-	SuratPemberitahuanPbb_Id *uint64     `json:"suratPemberitahuanPbb_id"`
-	Tanggal                  *string     `json:"tanggal"`
-	Keperluan                *string     `json:"keperluan"`
-	Petugas_User_Id          *uint64     `json:"petugas_user_id"`
-	Status                   Status      `json:"status"`
-	StatusKirim              StatusKirim `json:"statusKirim"`
-	NoSuratUndangan          *string     `json:"noSuratUndangan"`
-	Page                     int         `json:"page"`
-	PageSize                 int         `json:"page_size"`
+	JenisPajak      *int            `json:"jenisPajak"`
+	Tanggal         *time.Time      `json:"tanggal"`
+	Pukul           *datatypes.Time `json:"pukul"`
+	Tempat          *string         `json:"tempat"`
+	Keperluan       *string         `json:"keperluan"`
+	Petugas_User_Id *uint64         `json:"petugas_user_id"`
+	Status          *Status         `json:"status"`
+	StatusKirim     *StatusKirim    `json:"statusKirim"`
+	NoSuratUndangan *string         `json:"noSuratUndangan"`
+	Npwpd_Id        uint64          `json:"npwpd_id"`
+	Page            int             `json:"page"`
+	PageSize        int             `json:"page_size"`
 }
 
 type UpdateStatusTerbitDto struct {
