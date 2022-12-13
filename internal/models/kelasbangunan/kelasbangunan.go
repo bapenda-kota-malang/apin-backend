77c1822d673ead3a3e4db4befeb8094a4b1a5f79
K421Prabowo <kusuma.ari.prabowo@gmail.com>
2022-12-13 13:51:44 +0700

    update table kelas tanah dan bangunan

--- internal/models/kelasbangunan/kelasbangunan.go
+++ internal/models/kelasbangunan/kelasbangunan.go
@@ -4,6 +4,7 @@ import gh "github.com/bapenda-kota-malang/apin-backend/pkg/gormhelper"
 
 type KelasBangunan struct {
 	Id                      uint64   `json:"id" gorm:"primaryKey"`
+	KdBangunan              string   `json:"kdBangunan" gorm:"type:varchar(3)"`
 	NilaiMaxBangunan        *float64 `json:"nilaiMaxBangunan" gorm:"type:decimal(8,2)"`
 	NilaiMinBangunan        *float64 `json:"nilaiMinBangunan" gorm:"type:decimal(8,2)"`
 	NilaiPerM2Bangunan      *float64 `json:"nilaiPerM2Bangunan" gorm:"type:decimal(8,2)"`
@@ -13,6 +14,7 @@ type KelasBangunan struct {
 }
 
 type CreateDto struct {
+	KdBangunan              string   `json:"kdBangunan"`
 	NilaiMaxBangunan        *float64 `json:"nilaiMaxBangunan"`
 	NilaiMinBangunan        *float64 `json:"nilaiMinBangunan"`
 	NilaiPerM2Bangunan      *float64 `json:"nilaiPerM2Bangunan"`
@@ -21,6 +23,7 @@ type CreateDto struct {
 }
 
 type UpdateDto struct {
+	KdBangunan              string   `json:"kdBangunan"`
 	NilaiMaxBangunan        *float64 `json:"nilaiMaxBangunan"`
 	NilaiMinBangunan        *float64 `json:"nilaiMinBangunan"`
 	NilaiPerM2Bangunan      *float64 `json:"nilaiPerM2Bangunan"`
@@ -29,6 +32,7 @@ type UpdateDto struct {
 }
 
 type FilterDto struct {
+	KdBangunan              string   `json:"kdBangunan"`
 	NilaiMaxBangunan        *float64 `json:"nilaiMaxBangunan"`
 	NilaiMinBangunan        *float64 `json:"nilaiMinBangunan"`
 	NilaiPerM2Bangunan      *float64 `json:"nilaiPerM2Bangunan"`
