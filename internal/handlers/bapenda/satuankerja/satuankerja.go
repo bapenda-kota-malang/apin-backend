0570326ba131b13f94bf73bc69a0cfca2c7c10fd
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2022-12-26 07:05:10 +0700

    refactor things: change skpd to satuankerja #3

--- internal/handlers/bapenda/satuankerja/satuankerja.go
+++ internal/handlers/bapenda/satuankerja/satuankerja.go
@@ -6,7 +6,7 @@ import (
 	hh "github.com/bapenda-kota-malang/apin-backend/pkg/handlerhelper"
 
 	m "github.com/bapenda-kota-malang/apin-backend/internal/models/satuankerja"
-	s "github.com/bapenda-kota-malang/apin-backend/internal/services/skpd"
+	s "github.com/bapenda-kota-malang/apin-backend/internal/services/satuankerja"
 )
 
 type Crud struct{}
