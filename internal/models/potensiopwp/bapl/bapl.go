191a211b817bd0b39886a0303ad9355038948dfb
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2022-12-28 09:49:26 +0700

    fix pendataan-opawp: bapl + some subs

--- internal/models/potensiopwp/bapl/bapl.go
+++ internal/models/potensiopwp/bapl/bapl.go
@@ -11,39 +11,39 @@ import (
 )
 
 type PotensiBapl struct {
-	Id                  uint                   `json:"id" gorm:"primaryKey"`
-	TanggalPeninjauan   time.Time              `json:"tanggalPeninjauan"`
-	Potensiop_Id        uuid.UUID              `json:"potensiop_id" gorm:"type:uuid"`
-	CreateBy_User_Id    uint                   `json:"createBy_user_id"`
-	Koordinator_User_Id uint                   `json:"koordinator_user_id"`
-	Petugas_User_Id     *pq.Int64Array         `json:"petugas_user_id" gorm:"type:integer[]"`
-	Status              types.StatusVerifikasi `json:"status"`
-	Kasubid_User_Id     *uint                  `json:"kasubid_user_id,omitempty"`
-	Kabid_User_Id       *uint                  `json:"kabid_user_id,omitempty"`
+	Id                     uint                   `json:"id" gorm:"primaryKey"`
+	TanggalPeninjauan      time.Time              `json:"tanggalPeninjauan"`
+	Potensiop_Id           uuid.UUID              `json:"potensiop_id" gorm:"type:uuid"`
+	CreateBy_Pegawai_Id    uint                   `json:"createBy_pegawai_id"`
+	Koordinator_Pegawai_Id uint                   `json:"koordinator_pegawai_id"`
+	Petugas_Pegawai_Id     *pq.Int64Array         `json:"petugas_pegawai_id" gorm:"type:integer[]"`
+	Status                 types.StatusVerifikasi `json:"status"`
+	Kasubid_Pegawai_Id     *uint                  `json:"kasubid_pegawai_id,omitempty"`
+	Kabid_Pegawai_Id       *uint                  `json:"kabid_pegawai_id,omitempty"`
 	gormhelper.DateModel
-	CreateBy    *user.User `json:"createBy,omitempty" gorm:"foreignKey:CreateBy_User_Id"`
-	Koordinator *user.User `json:"koordinator,omitempty" gorm:"foreignKey:Koordinator_User_Id"`
-	Kasubid     *user.User `json:"kasubid,omitempty" gorm:"foreignKey:Kasubid_User_Id"`
-	Kabid       *user.User `json:"kabid,omitempty" gorm:"foreignKey:Kabid_User_Id"`
+	CreateBy    *user.User `json:"createBy,omitempty" gorm:"foreignKey:CreateBy_Pegawai_Id"`
+	Koordinator *user.User `json:"koordinator,omitempty" gorm:"foreignKey:Koordinator_Pegawai_Id"`
+	Kasubid     *user.User `json:"kasubid,omitempty" gorm:"foreignKey:Kasubid_Pegawai_Id"`
+	Kabid       *user.User `json:"kabid,omitempty" gorm:"foreignKey:Kabid_Pegawai_Id"`
 }
 
 type CreateDto struct {
-	TanggalPeninjauan   *time.Time             `json:"tanggalPeninjauan"`
-	Potensiop_Id        uuid.UUID              `json:"-"`
-	CreateBy_User_Id    uint                   `json:"-"`
-	Koordinator_User_Id uint                   `json:"koordinator_user_id"`
-	Petugas_User_Id     *pq.Int64Array         `json:"petugas_user_id"`
-	Status              types.StatusVerifikasi `json:"-"`
+	TanggalPeninjauan      *time.Time             `json:"tanggalPeninjauan"`
+	Potensiop_Id           uuid.UUID              `json:"-"`
+	CreateBy_Pegawai_Id    uint                   `json:"-"`
+	Koordinator_Pegawai_Id uint                   `json:"koordinator_pegawai_id"`
+	Petugas_Pegawai_Id     *pq.Int64Array         `json:"petugas_pegawai_id"`
+	Status                 types.StatusVerifikasi `json:"-"`
 }
 
 type UpdateDto struct {
-	TanggalPeninjauan   *time.Time     `json:"tanggalPeninjauan"`
-	Koordinator_User_Id *uint          `json:"koordinator_user_id"`
-	Petugas_User_Id     *pq.Int64Array `json:"petugas_user_id"`
+	TanggalPeninjauan      *time.Time     `json:"tanggalPeninjauan"`
+	Koordinator_Pegawai_Id *uint          `json:"koordinator_pegawai_id"`
+	Petugas_Pegawai_Id     *pq.Int64Array `json:"petugas_pegawai_id"`
 }
 
 type VerifyDto struct {
-	Status          string `json:"status" validate:"required"`
-	Kasubid_User_Id *uint  `json:"-"`
-	Kabid_User_Id   *uint  `json:"-"`
+	Status             string `json:"status" validate:"required"`
+	Kasubid_Pegawai_Id *uint  `json:"-"`
+	Kabid_Pegawai_Id   *uint  `json:"-"`
 }
