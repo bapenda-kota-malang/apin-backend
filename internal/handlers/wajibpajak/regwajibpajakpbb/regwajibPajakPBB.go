5df9d37311fed29488257bdbfa146c3374850cb9
K421Prabowo <kusuma.ari.prabowo@gmail.com>
2023-01-11 05:21:28 +0700

    update api be wp pelayanan

---
+++ internal/handlers/wajibpajak/regwajibpajakpbb/regwajibPajakPBB.go
@@ -0,0 +1,51 @@
+package wajibpajakpbb
+
+import (
+	"net/http"
+
+	hh "github.com/bapenda-kota-malang/apin-backend/pkg/handlerhelper"
+
+	m "github.com/bapenda-kota-malang/apin-backend/internal/models/regwajibpajakpbb"
+	s "github.com/bapenda-kota-malang/apin-backend/internal/services/regwajibpajakpbb"
+
+	ac "github.com/bapenda-kota-malang/apin-backend/pkg/apicore"
+	hj "github.com/bapenda-kota-malang/apin-backend/pkg/apicore/httpjson"
+	t "github.com/bapenda-kota-malang/apin-backend/pkg/apicore/types"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var input m.CreateDto
+	if hh.ValidateStructByIOR(w, r.Body, &input) == false {
+		return
+	}
+
+	result, err := s.Create(input, nil)
+	hh.DataResponse(w, result, err)
+}
+
+func GetList(w http.ResponseWriter, r *http.Request) {
+	var input m.FilterDto
+	if hh.ValidateStructByURL(w, *r.URL, &input) == false {
+		return
+	}
+
+	result, err := s.GetList(input)
+	hh.DataResponse(w, result, err)
+}
+
+func GetDetail(w http.ResponseWriter, r *http.Request) {
+	id := hh.ValidateAutoInc(w, r, "id")
+	if id < 1 {
+		return
+	}
+
+	result, err := s.GetDetail(id)
+	hh.DataResponse(w, result, err)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	data := t.II{
+		"message": "You are updateing wajibpajak of app: " + ac.Self.Name,
+	}
+	hj.WriteJSON(w, http.StatusOK, data, nil)
+}
