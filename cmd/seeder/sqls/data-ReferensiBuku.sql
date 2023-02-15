fab3d553c87a4c404364d8eba877ade8f67e1166
Hizkia Luke <contact.hizkia@gmail.com>
2023-02-15 10:18:37 +0700

    fix refBuku seeder

--- cmd/seeder/sqls/data-ReferensiBuku.sql
+++ cmd/seeder/sqls/data-ReferensiBuku.sql
@@ -20,7 +20,7 @@ SET row_security = off;
 -- Data for Name: ReferensiBuku; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."ReferensiBuku" (id, "Kode", "ThnAwal", "ThnAkhir", "NilaiMin", "NilaiMax", "Id", "LuasMinTipe", "LuasMaxTipe", "FaktorPembagiTipe", "Status", "CreatedAt", "UpdatedAt", "DeletedAt") FROM stdin;
+COPY public."ReferensiBuku" (Id, "Kode", "ThnAwal", "ThnAkhir", "NilaiMin", "NilaiMax", "Id", "LuasMinTipe", "LuasMaxTipe", "FaktorPembagiTipe", "Status", "CreatedAt", "UpdatedAt", "DeletedAt") FROM stdin;
 1	1	2000	2020	0	100000	1	\N	\N	\N	\N	\N	\N	\N
 2	2	2000	2020	100001	500000	2	\N	\N	\N	\N	\N	\N	\N
 3	3	2000	2020	500001	2000000	3	\N	\N	\N	\N	\N	\N	\N
@@ -37,10 +37,10 @@ SELECT pg_catalog.setval('public."ReferensiBuku_Id_seq"', 5, true);
 
 
 --
--- Name: ReferensiBuku_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: ReferensiBuku_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."ReferensiBuku_id_seq"', 1, false);
+SELECT pg_catalog.setval('public."ReferensiBuku_Id_seq"', 1, false);
 
 --
 -- PostgreSQL database dump complete
