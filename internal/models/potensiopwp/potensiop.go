8508ec15fd8e226c92ce60761f0f1ce7bdb23b48
Sultan Achmad <sultangendut@gmail.com>
2023-03-14 14:49:59 +0700

    fix: potensiopwp hiburan change to payload array fix: add rekening data get list potensi opwp

--- internal/models/potensiopwp/potensiop.go
+++ internal/models/potensiopwp/potensiop.go
@@ -18,44 +18,45 @@ import (
 )
 
 type PotensiOp struct {
-	Id             uuid.UUID       `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	Assessment     t.JenisPajak    `json:"assessment" gorm:"size:10"`
-	Golongan       t.Golongan      `json:"golongan"`
-	Npwp           *string         `json:"npwp" gorm:"size:50"`
-	Rekening_Id    uint64          `json:"rekening_id"`
-	StartDate      *datatypes.Date `json:"startDate"`
-	LuasBangunan   *string         `json:"luasBangunan" gorm:"size:50"`
-	JamBuka        *string         `json:"jamBuka" gorm:"size:50"`
-	JamTutup       *string         `json:"jamTutup" gorm:"size:50"`
-	Visitors       *string         `json:"visitors" gorm:"size:50"`
-	OmsetOp        float64         `json:"omsetOp"`
-	JumlahPajak    float64         `json:"jumlahPajak"`
-	TarifPajak_Id  *uint64         `json:"tarifPajak_id"`
-	Genset         bool            `json:"genset"`
-	AirTanah       bool            `json:"airTanah"`
-	User_Id        uint64          `json:"user_id"`
-	Status         t.Status        `json:"status"`
-	VendorEtax_Id  *uint64         `json:"vendorEtax_id"`
-	FotoKtp        *string         `json:"fotoKtp"`
-	FotoObjek      *pq.StringArray `json:"fotoObjek" gorm:"type:varchar[]"`
-	FormBapl       *string         `json:"formBapl"`
-	DokumenLainnya *pq.StringArray `json:"dokumenLainnya" gorm:"type:varchar[]"`
+	Id               uuid.UUID       `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	Assessment       t.JenisPajak    `json:"assessment" gorm:"size:10"`
+	Golongan         t.Golongan      `json:"golongan"`
+	Npwp             *string         `json:"npwp" gorm:"size:50"`
+	Rekening_Id      uint64          `json:"rekening_id"`
+	StartDate        *datatypes.Date `json:"startDate"`
+	LuasBangunan     *string         `json:"luasBangunan" gorm:"size:50"`
+	JamBuka          *string         `json:"jamBuka" gorm:"size:50"`
+	JamTutup         *string         `json:"jamTutup" gorm:"size:50"`
+	Visitors         *string         `json:"visitors" gorm:"size:50"`
+	OmsetOp          float64         `json:"omsetOp"`
+	JumlahPajak      float64         `json:"jumlahPajak"`
+	TarifPajak_Id    *uint64         `json:"tarifPajak_id"`
+	Genset           bool            `json:"genset"`
+	AirTanah         bool            `json:"airTanah"`
+	PajakKonsumenPct *float64        `json:"pajakKonsumenPcts"`
+	User_Id          uint64          `json:"user_id"`
+	Status           t.Status        `json:"status"`
+	VendorEtax_Id    *uint64         `json:"vendorEtax_id"`
+	FotoKtp          *string         `json:"fotoKtp"`
+	FotoObjek        *pq.StringArray `json:"fotoObjek" gorm:"type:varchar[]"`
+	FormBapl         *string         `json:"formBapl"`
+	DokumenLainnya   *pq.StringArray `json:"dokumenLainnya" gorm:"type:varchar[]"`
 	gormhelper.DateModel
 	Rekening   *rekening.Rekening     `json:"rekening,omitempty" gorm:"foreignKey:Rekening_Id"`
 	User       *user.User             `json:"user,omitempty" gorm:"foreignKey:User_Id"`
 	TarifPajak *tarifpajak.TarifPajak `json:"tarifPajak,omitempty" gorm:"foreignKey:TarifPajak_Id"`
 	// VendorEtax             cm.VendorEtax           `gorm:"foreignKey:VendorEtax_Id"`
-	DetailPotensiOp        *detailpotensiop.DetailPotensiOp       `json:"detailPotensiOp,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	PotensiPemilikWp       *[]potensipemilikwp.PotensiPemilikWp   `json:"potensiPemilikWp,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	PotensiNarahubung      *[]potensinarahubung.PotensiNarahubung `json:"potensiNarahubung,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiAirTanah  *DetailPotensiAirTanah                 `json:"potensiAirTanah,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiHiburan   *DetailPotensiHiburan                  `json:"potensiHiburan,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiHotels    *[]DetailPotensiHotel                  `json:"potensiHotels,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiParkirs   *[]DetailPotensiParkir                 `json:"potensiParkirs,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiPPJNonPLN *DetailPotensiPPJNonPLN                `json:"potensiPpjNonPLN,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiReklames  *[]DetailPotensiReklame                `json:"potensiReklames,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	DetailPotensiResto     *DetailPotensiResto                    `json:"potensiResto,omitempty" gorm:"foreignKey:Potensiop_Id"`
-	Bapl                   *[]bapl.PotensiBapl                    `json:"bapl,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiOp        *detailpotensiop.DetailPotensiOp      `json:"detailPotensiOp,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	PotensiPemilikWp       []potensipemilikwp.PotensiPemilikWp   `json:"potensiPemilikWp,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	PotensiNarahubung      []potensinarahubung.PotensiNarahubung `json:"potensiNarahubung,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiAirTanah  *DetailPotensiAirTanah                `json:"potensiAirTanah,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiHiburans  []DetailPotensiHiburan                `json:"potensiHiburans,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiHotels    []DetailPotensiHotel                  `json:"potensiHotels,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiParkirs   []DetailPotensiParkir                 `json:"potensiParkirs,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiPPJNonPLN *DetailPotensiPPJNonPLN               `json:"potensiPpjNonPLN,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiReklames  []DetailPotensiReklame                `json:"potensiReklames,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	DetailPotensiResto     *DetailPotensiResto                   `json:"potensiResto,omitempty" gorm:"foreignKey:Potensiop_Id"`
+	Bapl                   []bapl.PotensiBapl                    `json:"bapl,omitempty" gorm:"foreignKey:Potensiop_Id"`
 }
 
 type CreatePotensiOpDto struct {
@@ -74,6 +75,7 @@ type CreatePotensiOpDto struct {
 	TarifPajak_Id  *uint64         `json:"-"`
 	Genset         bool            `json:"genset"`
 	AirTanah       bool            `json:"airTanah"`
+	PajakKonsumen  *float64        `json:"pajakKonsumen"`
 	User_Id        uint64          `json:"user_id"`
 	Status         t.Status        `json:"status"`
 	VendorEtax_Id  *uint64         `json:"vendorEtax_id"`
